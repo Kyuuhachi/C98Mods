107cf2bedc522d3f029c38c532c21f319760a0bc
Caagr98 <Caagr98@users.noreply.github.com>
2015-04-05 21:24:00 +0200

    Fugbixes or whatever

--- C98Core/c98/core/impl/asm/render/C98GuiIngame.java
+++ C98Core/c98/core/impl/asm/render/C98GuiIngame.java
@@ -1,18 +1,23 @@
 package c98.core.impl.asm.render;
 
 import java.io.IOException;
+import java.util.Deque;
+import java.util.LinkedList;
 import jdk.internal.org.objectweb.asm.ClassReader;
-import jdk.internal.org.objectweb.asm.Opcodes;
 import jdk.internal.org.objectweb.asm.tree.*;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiIngame;
+import net.minecraft.profiler.Profiler;
 import c98.core.launch.*;
 
 @ASMer("net/minecraftforge/client/GuiIngameForge") class C98GuiIngame extends GuiIngame implements CustomASMer {
 	
 	class FieldName {
+		Profiler p;
 		{
-			String.valueOf(Minecraft.getMinecraft().gameSettings.showDebugInfo);
+			p.startSection(null);
+			p.endStartSection(null);
+			p.endSection();
 		}
 	}
 	
@@ -21,23 +26,50 @@ import c98.core.launch.*;
 	}
 	
 	@Override public void asm(ClassNode node) {
-		FieldInsnNode showDebug = null;
+		MethodInsnNode start = null, endstart = null, end = null;
 		try {
 			ClassNode clz = new ClassNode();
 			new ClassReader(getClass().getName() + "$FieldName").accept(clz, 0);
-			for(AbstractInsnNode n : new Asm(clz.methods.get(0)))
-				if(n instanceof FieldInsnNode) showDebug = (FieldInsnNode)n;
+			for(AbstractInsnNode n : new Asm(clz.methods.get(0))) {
+				if(!(n instanceof MethodInsnNode)) continue;
+				MethodInsnNode m = (MethodInsnNode)n;
+				if(start == null) start = m;
+				else if(endstart == null) endstart = m;
+				else if(end == null) end = m;
+			}
 		} catch(IOException e) {}
 		
 		for(MethodNode method : node.methods)
 			for(AbstractInsnNode insn : new Asm(method))
-				if(insn instanceof FieldInsnNode) {
-					FieldInsnNode n = (FieldInsnNode)insn;
-					if(n.owner.equals(showDebug.owner) && n.name.equals(showDebug.name)) {
-						method.instructions.insert(insn, new MethodInsnNode(Opcodes.INVOKESTATIC, "c98/core/impl/HookImpl", "renderHud", "()V", false));
-						return;
+				if(insn instanceof MethodInsnNode) {
+					MethodInsnNode n = (MethodInsnNode)insn;
+					if(eq(n, start)) start(node, getPrev(n));
+					if(eq(n, endstart)) {
+						end(node, popPrev());
+						start(node, getPrev(n));
 					}
+					if(eq(n, end)) end(node, popPrev());
 				}
 		
+	}
+	
+	private Deque<String> stack = new LinkedList();
+	
+	private String popPrev() {
+		return stack.pop();
+	}
+	
+	private String getPrev(AbstractInsnNode n) {
+		String str = null;
+		stack.push(str);
+		return str;
+	}
+	
+	private static void start(ClassNode node, String name) {}
+	
+	private static void end(ClassNode node, String name) {}
+	
+	private static boolean eq(MethodInsnNode a, MethodInsnNode b) {
+		return a.owner.equals(b.owner) && a.name.equals(b.name) && a.desc.equals(b.desc);
 	}
 }

1d50ac4351876e621be8d4bfff328f2c8f7e1cf9
Caagr98 <Caagr98@users.noreply.github.com>
2015-04-24 14:23:17 +0200

    Made cavemap crash less - now it neither crashes itself or the launcher.

--- C98Mods/c98/minemap/maptype/CaveMap.java
+++ C98Mods/c98/minemap/maptype/CaveMap.java
@@ -1,7 +1,7 @@
 package c98.minemap.maptype;
 
-import net.minecraft.block.Block;
 import net.minecraft.block.material.MapColor;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.BlockPos;
 import net.minecraft.world.chunk.Chunk;
 import c98.minemap.api.MapHandler;
@@ -10,45 +10,32 @@ public class CaveMap extends MapHandler {
 	@Override public int calc(Chunk chunk, int x, int z, int plY) {
 		boolean down = false;
 		boolean up = false;
-		Block id = null;
 		
-		if(plY > 1) {
+		if(plY >= 0) {
 			boolean visibleBlockFound;
 			
 			do {
-				visibleBlockFound = true;
-				id = chunk.getBlock(x, plY - 1, z);
-				
-				if(id == null) visibleBlockFound = false;
-				else if(plY > 0 && id.getMapColor(chunk.getBlockState(new BlockPos(x, plY - 1, z))) == MapColor.airColor) visibleBlockFound = false;
+				visibleBlockFound = !isAir(chunk, x, plY, z);
 				
 				if(!visibleBlockFound) {
 					down = true;
 					--plY;
-					
-					if(plY <= 0) break;
-					
-					id = chunk.getBlock(x, plY - 1, z);
 				}
-			} while(plY > 0 && !visibleBlockFound);
-			if(!down) {
-				Block block = chunk.getBlock(x, plY, z);
-				if(block == null || block.getMapColor(chunk.getBlockState(new BlockPos(x, plY, z))) == MapColor.airColor) up = true;
-			}
-			
+			} while(plY >= 0 && !visibleBlockFound);
+			if(!down) up = isAir(chunk, x, plY + 1, z);
 		}
 		byte brightness = 1;
 		if(up) brightness = 2;
 		if(down) brightness = 0;
-		int color = 0;
 		
-		if(id != null) {
-			MapColor materialColor = id.getMapColor(chunk.getBlockState(new BlockPos(x, plY, z)));
-			
-			color = materialColor.colorIndex;
-		}
-		
+		IBlockState block = chunk.getBlockState(new BlockPos(x, plY, z));
+		int color = block.getBlock().getMapColor(block).colorIndex;
 		return getColor((byte)(color * 4 + brightness), x + z & 1);
 		
+	}
+	
+	private static boolean isAir(Chunk chunk, int x, int plY, int z) {
+		IBlockState block = chunk.getBlockState(new BlockPos(x, plY, z));
+		return block == null || block.getBlock().getMapColor(block) == MapColor.airColor;
 	}
 }

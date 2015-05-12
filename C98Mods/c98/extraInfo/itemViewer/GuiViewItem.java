ad16d22ddbdfc9b3c7ed383c6d5d54c7f6f175bd
Caagr98 <Caagr98@users.noreply.github.com>
2015-05-12 11:11:15 +0200

    Updated ItemViewer to use Jackson instead of Gson

--- C98Mods/c98/extraInfo/itemViewer/GuiViewItem.java
+++ C98Mods/c98/extraInfo/itemViewer/GuiViewItem.java
@@ -15,8 +15,9 @@ import net.minecraft.util.*;
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.input.Mouse;
 import c98.core.GL;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.*;
 import com.google.common.base.Splitter;
-import com.google.gson.*;
 
 public class GuiViewItem extends GuiScreen {
 	private static final int WIDTH = 256, HEIGHT = 202;
@@ -26,7 +27,7 @@ public class GuiViewItem extends GuiScreen {
 	private static final int SCROLL_W = 12, SCROLL_H = 153;
 	private static final int SCROLL_KNOB_H = 15;
 	
-	private static final int ROWS = FIELD_H / 9;
+	private static final int LINES = FIELD_H / 9;
 	
 	private static final ChatStyle bracket = new ChatStyle().setColor(EnumChatFormatting.AQUA);
 	private static final ChatStyle squarebracket = new ChatStyle().setColor(EnumChatFormatting.BLUE);
@@ -52,52 +53,55 @@ public class GuiViewItem extends GuiScreen {
 		writeJsonElement(toJson(nbt), 0, newline(0), "");
 		
 		text = components.toArray(new IChatComponent[0]);
-		maxScroll = text.length - ROWS;
+		maxScroll = text.length - LINES;
 		if(maxScroll < 0) maxScroll = 0;
 	}
 	
-	private IChatComponent writeJsonElement(JsonElement j, int indent, IChatComponent comp, String path) {
-		if(j instanceof JsonObject) return writeJsonObject((JsonObject)j, indent, comp, path);
-		if(j instanceof JsonArray) return writeJsonArray((JsonArray)j, indent, comp, path);
-		return writeJsonPrimitive(j, indent, comp, path);
+	private IChatComponent writeJsonElement(JsonNode j, int indent, IChatComponent comp, String path) {
+		if(j instanceof ObjectNode) return writeJsonObject((ObjectNode)j, indent, comp, path);
+		if(j instanceof ArrayNode) return writeJsonArray((ArrayNode)j, indent, comp, path);
+		return writeJsonPrimitive((ValueNode)j, indent, comp, path);
 	}
 	
-	private IChatComponent writeJsonObject(JsonObject j, int indent, IChatComponent comp, String path) {
+	private IChatComponent writeJsonObject(ObjectNode j, int indent, IChatComponent comp, String path) {
 		comp.appendSibling(comp("{", bracket));
-		List<Map.Entry<String, JsonElement>> entries = new ArrayList(j.entrySet());
-		for(int i = 0; i < entries.size(); i++) {
-			Map.Entry<String, JsonElement> e = entries.get(i);
+		Iterator<Map.Entry<String, JsonNode>> entries = j.fields();
+		while(entries.hasNext()) {
+			Map.Entry<String, JsonNode> e = entries.next();
 			comp = newline(indent + 1);
 			IChatComponent val = comp(e.getKey(), key).appendSibling(comp(": ", punctuation));
 			comp.appendSibling(val);
 			comp = writeJsonElement(e.getValue(), indent + 1, val, path + e.getKey() + "/");
-			if(i != entries.size() - 1) comp.appendSibling(comp(",", punctuation));
+			if(entries.hasNext()) comp.appendSibling(comp(",", punctuation));
 		}
 		return newline(indent).appendSibling(comp("}", bracket));
 	}
 	
-	private IChatComponent writeJsonArray(JsonArray j, int indent, IChatComponent comp, String path) {
+	private IChatComponent writeJsonArray(ArrayNode j, int indent, IChatComponent comp, String path) {
 		comp.appendSibling(comp("[", squarebracket));
-		for(int i = 0; i < j.size(); i++) {
-			JsonElement e = j.get(i);
+		Iterator<JsonNode> entries = j.iterator();
+		while(entries.hasNext()) {
+			JsonNode e = entries.next();
 			comp = newline(indent + 1);
 			comp = writeJsonElement(e, indent + 1, comp, path + "[]/");
-			if(i != j.size() - 1) comp.appendSibling(comp(",", punctuation));
+			if(entries.hasNext()) comp.appendSibling(comp(",", punctuation));
 		}
 		return newline(indent).appendSibling(comp("]", squarebracket));
 	}
 	
-	private static IChatComponent writeJsonPrimitive(JsonElement j, int indent, IChatComponent comp, String path) {
+	private static IChatComponent writeJsonPrimitive(ValueNode j, int indent, IChatComponent comp, String path) {
 		IChatComponent val = null;
-		if(j.isJsonNull()) val = comp("null", keyword);
+		if(j.isNull()) val = comp("null", keyword);
 		else {
-			JsonPrimitive p = (JsonPrimitive)j;
-			if(p.isString()) val = comp(j.getAsString(), string);
-			if(p.isBoolean()) val = comp(j.getAsString(), keyword);
-			if(p.isNumber()) val = comp(j.getAsString(), number);
+			if(j.isTextual()) val = comp(j.asText(), string);
+			if(j.isBoolean()) val = comp(j.asText(), keyword);
+			if(j.isNumber()) val = comp(j.asText(), number);
 		}
 		comp.appendSibling(val);
-		if(path.endsWith("tag/ench/[]/id/") && j.isJsonPrimitive() && ((JsonPrimitive)j).isNumber()) comp.getChatStyle().setChatHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, comp(StatCollector.translateToLocal(Enchantment.enchantmentsList[j.getAsInt()].getName()), new ChatStyle())));
+		if(path.endsWith("tag/ench/[]/id/") && j.isNumber()) {
+			IChatComponent tooltip = comp(StatCollector.translateToLocal(Enchantment.enchantmentsList[j.asInt()].getName()), new ChatStyle());
+			comp.getChatStyle().setChatHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, tooltip));
+		}
 		return comp;
 	}
 	
@@ -114,42 +118,42 @@ public class GuiViewItem extends GuiScreen {
 		return c;
 	}
 	
-	private JsonElement toJson(NBTBase nbt) {
+	private JsonNode toJson(NBTBase nbt) {
 		switch(NBTBase.NBT_TYPES[nbt.getId()]) {
 			case "BYTE":
-				return new JsonPrimitive(((NBTTagByte)nbt).getByte());
+				return new ShortNode(((NBTTagByte)nbt).getByte());
 			case "SHORT":
-				return new JsonPrimitive(((NBTTagShort)nbt).getShort());
+				return new ShortNode(((NBTTagShort)nbt).getShort());
 			case "INT":
-				return new JsonPrimitive(((NBTTagInt)nbt).getInt());
+				return new IntNode(((NBTTagInt)nbt).getInt());
 			case "LONG":
-				return new JsonPrimitive(((NBTTagLong)nbt).getLong());
+				return new LongNode(((NBTTagLong)nbt).getLong());
 			case "FLOAT":
-				return new JsonPrimitive(((NBTTagFloat)nbt).getFloat());
+				return new FloatNode(((NBTTagFloat)nbt).getFloat());
 			case "DOUBLE":
-				return new JsonPrimitive(((NBTTagDouble)nbt).getDouble());
+				return new DoubleNode(((NBTTagDouble)nbt).getDouble());
 			case "STRING":
-				return new JsonPrimitive(((NBTTagString)nbt).getString());
+				return new TextNode(((NBTTagString)nbt).getString());
 			case "BYTE[]": {
-				JsonArray ar = new JsonArray();
+				ArrayNode ar = new ArrayNode(new JsonNodeFactory(false));
 				for(byte b : ((NBTTagByteArray)nbt).getByteArray())
-					ar.add(new JsonPrimitive(b));
+					ar.add(b);
 				return ar;
 			}
 			case "INT[]": {
-				JsonArray ar = new JsonArray();
+				ArrayNode ar = new ArrayNode(new JsonNodeFactory(false));
 				for(int b : ((NBTTagIntArray)nbt).getIntArray())
-					ar.add(new JsonPrimitive(b));
+					ar.add(b);
 				return ar;
 			}
 			case "COMPOUND": {
-				JsonObject o = new JsonObject();
+				ObjectNode o = new ObjectNode(new JsonNodeFactory(false));
 				for(String name : (Iterable<String>)((NBTTagCompound)nbt).getKeySet())
-					o.add(name, toJson(((NBTTagCompound)nbt).getTag(name)));
+					o.set(name, toJson(((NBTTagCompound)nbt).getTag(name)));
 				return o;
 			}
 			case "LIST": {
-				JsonArray ar = new JsonArray();
+				ArrayNode ar = new ArrayNode(new JsonNodeFactory(false));
 				for(int i = 0; i < ((NBTTagList)nbt).tagCount(); i++)
 					ar.add(toJson((NBTBase)((NBTTagList)nbt).tagList.get(i)));
 				return ar;
@@ -175,7 +179,7 @@ public class GuiViewItem extends GuiScreen {
 		
 		int x = FIELD_X;
 		int y = FIELD_Y;
-		for(int i = scroll; i < scroll + ROWS && i < text.length; i++, y += mc.fontRendererObj.FONT_HEIGHT) {
+		for(int i = scroll; i < scroll + LINES && i < text.length; i++, y += mc.fontRendererObj.FONT_HEIGHT) {
 			GL.color(1, 1, 1);
 			mc.fontRendererObj.drawString(mc.fontRendererObj.trimStringToWidth(text[i].getFormattedText(), FIELD_W), x, y, 0xAFAFAF);
 			if(mouseY >= y && mouseY < y + mc.fontRendererObj.FONT_HEIGHT) {
@@ -185,7 +189,6 @@ public class GuiViewItem extends GuiScreen {
 					if(compX > mouseX) {
 						HoverEvent e = c.getChatStyle().getChatHoverEvent();
 						if(e != null) drawHoveringText(Splitter.on("\n").splitToList(e.getValue().getFormattedText()), mouseX, mouseY);
-//							GL.disableLighting(); TODO is this needed?
 						break;
 					}
 				}
@@ -214,8 +217,8 @@ public class GuiViewItem extends GuiScreen {
 		super.keyTyped(par1, par2);
 		if(par2 == Keyboard.KEY_UP) scroll--;
 		if(par2 == Keyboard.KEY_DOWN) scroll++;
-		if(par2 == Keyboard.KEY_PRIOR) scroll -= ROWS - 3;
-		if(par2 == Keyboard.KEY_NEXT) scroll += ROWS - 3;
+		if(par2 == Keyboard.KEY_PRIOR) scroll -= LINES - 3;
+		if(par2 == Keyboard.KEY_NEXT) scroll += LINES - 3;
 		
 		scroll();
 	}

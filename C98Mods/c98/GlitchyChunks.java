62f0a02fa14c5c95b5356716c5db3b807f0ea9d7
Caagr98 <Caagr98@users.noreply.github.com>
2015-09-08 16:25:16 +0200

    Remove unnecessary reflection

--- C98Mods/c98/GlitchyChunks.java
+++ C98Mods/c98/GlitchyChunks.java
@@ -1,8 +1,6 @@
 package c98;
 
-import java.lang.reflect.Constructor;
 import net.minecraft.world.WorldType;
-import c98.core.C98Log;
 import c98.core.C98Mod;
 
 public class GlitchyChunks extends C98Mod {
@@ -10,12 +8,6 @@ public class GlitchyChunks extends C98Mod {
 	public static final int num = 64;
 	
 	@Override public void load() {
-		try {
-			Constructor<WorldType> ctor = WorldType.class.getDeclaredConstructor(int.class, String.class);
-			ctor.setAccessible(true);
-			TYPE = ctor.newInstance(7, "glitch");
-		} catch(Exception e) {
-			C98Log.error("Failed to create Glitchy world type", e);
-		}
+		TYPE = new WorldType(7, "glitch");
 	}
 }

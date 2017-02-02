2a406853d8bd0a63a6cfb520d3f8b6b4da1de9e2
Caagr98 <caagr98@gmail.com>
2017-02-02 11:50:09 +0100

    Remove unused import

--- src/main/java/c98/minemap/maptype/CaveMap.java
+++ src/main/java/c98/minemap/maptype/CaveMap.java
@@ -3,7 +3,6 @@ package c98.minemap.maptype;
 import c98.minemap.api.MapHandler;
 
 import net.minecraft.block.material.MapColor;
-import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.math.BlockPos.MutableBlockPos;
 import net.minecraft.world.World;
 
